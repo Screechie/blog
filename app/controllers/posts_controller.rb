ca2023419c06c0bc5da46a5b056f4b7464d7057d
Screechie <simeonbourne@gmail.com>
2015-04-06 03:05:18 -0400

    Add validations to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,25 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all.order('created_at DESC')#List all posts on index page with newest first
+		#List all posts on index page with newest first
+		#Documented by S.Bourne: 04/06/2015
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
 
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			#Render used to avoid loss of content.If a redirect is done a new http request is initiated
+			#and content is destroyed.
+			#Documented by S.Bourne:04/06/2015
+			render 'new'
+		end
 	end
 
 	def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	#Validation to avoid blank posts from being submitted
+	#Documented by S.Bourne: 04/06/2015
+	validates :title, presence: true, length: {minimum: 5 }
+	validates :body, presence: true
 end
