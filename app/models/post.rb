1542ac592138888edfe8799a0c675558c775f572
Screechie <simeonbourne@gmail.com>
2015-04-06 04:34:49 -0400

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
 	#Validation to avoid blank posts from being submitted
 	#Documented by S.Bourne: 04/06/2015
 	validates :title, presence: true, length: {minimum: 5 }
